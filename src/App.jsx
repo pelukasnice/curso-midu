7056bf5ba37ba380ba8fb4ee8792967275421300
pelukasnice <103325490+pelukasnice@users.noreply.github.com>
2025-03-14 16:59:53 -0300

    App.jsx: componente basico listo

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+export function App(){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/github/mdo" alt="User Avatar" />
+                <div>
+                    <strong>El Barba Mcqueen</strong>
+                    <span>@elBarba</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
+
+
+
+
