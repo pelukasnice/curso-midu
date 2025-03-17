688fa1b90050ffa2f19ae5149157e6e41aad31a0
pelukasnice <103325490+pelukasnice@users.noreply.github.com>
2025-03-17 12:38:16 -0300

    se agrega funcion formatUserName la cual agrega el @

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
+    const formatUserName = (userName) =>`@${userName}`
     return (
-        <>
-            <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />
-            <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />
-            <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />    
-        </>
+        <section className='App'>
+            <TwitterFollowCard 
+            formatUserName={formatUserName} 
+            isFollowing 
+            userName="midudev" 
+            name="Miguel Angel Duran" />
+
+            <TwitterFollowCard 
+            formatUserName={formatUserName} 
+            isFollowing={false} 
+            userName="Donald_Trump" 
+            name="Donald Trump" />
+
+            <TwitterFollowCard 
+            formatUserName={formatUserName} 
+            isFollowing 
+            userName="ElonMusk" 
+            name="Elon Musck" />    
+        </section>
     )
 }
 
