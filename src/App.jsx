94504d531e684255674cfc42a6141e51fdd8857c
pelukasnice <103325490+pelukasnice@users.noreply.github.com>
2025-03-17 12:02:58 -0300

    TwitterFollowCard: se modifica react.fragment a <>

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
     return (
-        <React.Fragment>
+        <>
             <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />
             <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />
             <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />    
-        </React.Fragment>
+        </>
     )
 }
 
