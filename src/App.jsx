e1e2a569167e9f03316ab2be9734d6fa6ffb77af
pelukasnice <103325490+pelukasnice@users.noreply.github.com>
2025-03-17 15:19:12 -0300

    componente terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,45 @@ import React from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: false
+    },
+    {
+        userName: 'Donald_Trump',
+        name: 'Donald Trump',
+        isFollowing:false
+    },
+    {
+        userName: 'ElonMusk',
+        name: 'Elon Musk',
+        isFollowing:true
+    }
+]
+
 export function App() {
     const format = (userName) => `@${userName}`
 
     return (
         <section className='App'>
-
-            <TwitterFollowCard formatUserName={format}  userName="midudev">
-                Miguel Angel Duran
-            </TwitterFollowCard>
-
-            <TwitterFollowCard formatUserName={format}  userName="Donald_Trump">
-                Donad Trump
-            </TwitterFollowCard>
-
-            <TwitterFollowCard formatUserName={format}  userName="ElonMusk">
-                Elon Musk
-            </TwitterFollowCard>
+            {
+                users.map(user =>{
+                    const {userName, name, isFollowing} = user
+                    return (
+                        <TwitterFollowCard
+                        key={userName}// para que react sepa que es un componente unico.
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                        formatUserName={format}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+            
         </section>
     )
 }
